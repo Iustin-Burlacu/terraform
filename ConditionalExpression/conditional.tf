18d35acccc57c15277dde25c5d7ef186626d36fc
Iustin-Burlacu <50848825+Iustin-Burlacu@users.noreply.github.com>
2024-04-24 16:25:09 +0200

    modules logic

---
+++ ConditionalExpression/conditional.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+variable "istest" {}
+
+resource "aws_instance" "dev" {
+    ami = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
+    count = var.istest == true ? 1: 0
+}
+
+resource "aws_instance" "prod" {
+    ami = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
+    count = var.istest == false ? 1: 0
+}
